3700583dbc955808f76428e8e808683682cb3278
ravindu.shalika <shalika@ideahub.lk>
2024-07-23 19:06:08 +0530

    initial commit

---
+++ spring-boot-microservices-initial-setup/inventory-service/src/main/java/com/programmingtechie/inventoryservice/service/InventoryService.java
@@ -0,0 +1,30 @@
+package com.programmingtechie.inventoryservice.service;
+
+import com.programmingtechie.inventoryservice.dto.InventoryResponse;
+import com.programmingtechie.inventoryservice.model.Inventory;
+import com.programmingtechie.inventoryservice.repository.InventoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+@Service
+@RequiredArgsConstructor
+public class InventoryService {
+
+    private final InventoryRepository inventoryRepository;
+
+    @Transactional(readOnly = true)
+    public List<InventoryResponse> isInStock(List<String> skuCodes) {
+        return inventoryRepository.findBySkuCodeIn(skuCodes).stream()
+                .map(Inventory ->
+                    InventoryResponse.builder()
+                            .SkuCode(Inventory.getSkuCode())
+                            .isInStock(Inventory.getQuantity() > 0)
+                            .build()
+                ).toList();
+    }
+}
